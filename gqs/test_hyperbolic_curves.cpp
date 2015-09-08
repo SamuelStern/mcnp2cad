ab8262fd2e30b6db150fe948f2755583be080cf8
pshriwise <shriwise@wisc.edu>
2015-09-08 15:34:55 -0500

    Implemented a rudimentary test system for the hyperbolic curves.

--- gqs/test_hyperbolic_curves.cpp
+++ gqs/test_hyperbolic_curves.cpp
@@ -2,6 +2,27 @@
 
 #include "gq.hpp"
 
+
+#define FLAG_ERROR throw 1
+
+inline void flag_error() { FLAG_ERROR; }
+
+#define EQUAL_TEST( TEST, TYPE ) if( !(TEST) ) {	\
+  printf( "Equality Test Failed: \n"); \
+  printf( "  Expected value: %" #TYPE "\n", A ); \
+  printf( "  Actual value:   %" #TYPE "\n", B ); \
+  printf( "\n" ); \
+  flag_error();	  \
+  }
+
+#define CHECK_EQUAL( EXP, ACT ) check_equal ( (EXP), (ACT) ) 
+
+
+bool check_equal(int A, int B) { EQUAL_TEST( A == B, d) };
+bool check_equal(double A, double B) { EQUAL_TEST( A == B, f) };
+
+
+
 int main()
 {
 
@@ -15,15 +36,17 @@ int main()
   hyperbolic_curves(A,B,edges);
 
   
-  BasicTopologyEntity* bte = dynamic_cast<BasicTopologyEntity*>(edges[0]);
-
-  CubitBox edge_box = bte->bounding_box();
+  BasicTopologyEntity* bte1 = dynamic_cast<BasicTopologyEntity*>(edges[0]);
+  BasicTopologyEntity* bte2 = dynamic_cast<BasicTopologyEntity*>(edges[1]);
+    
+  CubitBox edge1_box = bte1->bounding_box();
+  CubitBox edge2_box = bte2->bounding_box();
+  
 
   //check the bounds of each curve
 
   ////// Curve 1 Check ///////
-  
-  
+  CHECK_EQUAL( 2.0 , edge1_box.max_x() );
 
 
 
