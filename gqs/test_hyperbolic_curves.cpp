ba3a7e3cd593d3926fe5a70316865e6cf6dfbc76
pshriwise <shriwise@wisc.edu>
2015-09-08 23:44:34 -0500

    Updated test values.

--- gqs/test_hyperbolic_curves.cpp
+++ gqs/test_hyperbolic_curves.cpp
@@ -16,12 +16,14 @@ inline void flag_error() { FLAG_ERROR; }
   }
 
 #define CHECK_EQUAL( EXP, ACT ) check_equal ( (EXP), (ACT) ) 
-
+#define CHECK_REAL_EQUAL( EXP, ACT, EPS ) check_equal ( (EXP), (ACT), (EPS))
 
 bool check_equal(int A, int B) { EQUAL_TEST( A == B, d) };
 bool check_equal(double A, double B) { EQUAL_TEST( A == B, f) };
+bool check_equal(double A, double B, double eps) { EQUAL_TEST( (A-B < eps), f ) };
 
 
+double tol = 1e-6;
 
 int main()
 {
@@ -41,22 +43,36 @@ int main()
     
   CubitBox edge1_box = bte1->bounding_box();
   CubitBox edge2_box = bte2->bounding_box();
-  
 
   //check the bounds of each curve
 
   ////// Curve 1 Check ///////
-  // CHECK_EQUAL( 2.0 , edge1_box.max_x() );
-  // CHECK_EQUAL( 1.0 , edge1_box.min_x() );
-
-  //should be created by now, time to export
-  DLIList<RefEntity*> exp_bodies;
-  int exp_ents;
-  CubitString cubit_version("12.2");
+  CubitVector expected_midpoint(1.0,0.0,0.0);
+  CubitVector actual_midpoint;
   
-  CubitCompat_export_solid_model(exp_bodies, "test_file.sat", "ACIS_SAT", exp_ents, cubit_version);
+  edges[0]->mid_point(actual_midpoint);
+  CHECK_REAL_EQUAL ( 1.0, actual_midpoint[0], tol );
+  
+  //should be created by now, time to export
+  if (false)
+    {
+      DLIList<RefEntity*> exp_bodies;
+      int exp_ents;
+      CubitString cubit_version("12.2");
+      CubitCompat_export_solid_model(exp_bodies, "test_file.sat", "ACIS_SAT", exp_ents, cubit_version);
+    }
 
 
+
+}
+
+
+void check_CubitVector( CubitVector A, CubitVector B )
+{
+
+  CHECK_REAL_EQUAL( A[0], B[0], tol);
+  CHECK_REAL_EQUAL( A[1], B[1], tol);
+  CHECK_REAL_EQUAL( A[2], B[2], tol);
 
 
 }
