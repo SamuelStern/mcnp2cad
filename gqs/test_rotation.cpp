59c7d0c8e80ea43fb56920ec1f5f6e5ef796f569
pshriwise <shriwise@wisc.edu>
2015-09-14 16:08:10 -0500

    Added test for negative rotation value.

--- gqs/test_rotation.cpp
+++ gqs/test_rotation.cpp
@@ -50,6 +50,20 @@ int main()
   CHECK_REAL_EQUAL( 90.0, z, CHECK_TOLERANCE);
   
   
+  //TEST 4 - Parabolic Cylinder
+  A = 1.0; B = 1.0; C = 1.0;
+  D = -1.0; E = 0.0; F = 0.0;
+  G = 0.0; H = 0.0; J = 0.0;
+  K = 9.0;
+
+  get_rotation(A,B,C,D,E,F,y,z);
+
+  CHECK_REAL_EQUAL( 1.5, A, CHECK_TOLERANCE);
+  CHECK_REAL_EQUAL( 0.5, B, CHECK_TOLERANCE);
+  CHECK_REAL_EQUAL( 0.5, C, CHECK_TOLERANCE);
+  CHECK_REAL_EQUAL( 9.0, K, CHECK_TOLERANCE);
+  CHECK_REAL_EQUAL( 0.0, y, CHECK_TOLERANCE);
+  CHECK_REAL_EQUAL( -45.0, z, CHECK_TOLERANCE);
 
 
   //if we get to this point, all of the tests have passed
